a0705ae1cb3801a21e7ba54fc999fcf33ba44eae
markostivicic <m.stivicic@gmail.com>
2023-07-07 08:29:35 +0200

    add employee models

---
+++ TestSourcetree/TestSourcetree.api/Models/CreateEmployeeRest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Mono_projekt.webapi.Models
+{
+    public class CreateEmployeeRest
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
---
+++ TestSourcetree/TestSourcetree.api/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mono_projekt.webapi.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Employee(Guid id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
